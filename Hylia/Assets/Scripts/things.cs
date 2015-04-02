931ab4c9dec666dbb68a3e3784eb8716c77802d0
kondorriano <nicobito@gmail.com>
2015-04-02 22:16:34 +0200

    Added Light asset and DivisionCamera Algorithm

---
+++ Hylia/Assets/Scripts/things.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class things : MonoBehaviour {
+
+	MeshCollider mC;
+	// Use this for initialization
+	void Start () {
+		mC = GameObject.FindGameObjectWithTag ("Player").GetComponent<MeshCollider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray test = new Ray(transform.position, -transform.forward);
+		Debug.DrawRay (transform.position, -transform.forward*10f);
+
+		if(mC.Raycast(test, out hit, 10f)) {
+			Debug.Log(hit.collider.gameObject.name);
+		}
+
+
+	}
+}
