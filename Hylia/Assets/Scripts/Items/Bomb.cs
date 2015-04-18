5382077d726f5127a59da7c575f1e1d8c0826c84
SergiCebrianGres <superlistor@hotmail.com>
2015-04-18 16:56:14 +0200

    Bombs Prefab & Scripts Added, BombTest Scene added.
    Primeriiiiisima version de la bomba

---
+++ Hylia/Assets/Scripts/Items/Bomb.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bomb : MonoBehaviour {
+	public float timer = 3.0f;
+	public GameObject explosion;
+
+	Transform myPlayer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > 0.0f ){
+			timer -= Time.deltaTime;
+		}
+		if (timer <= 0.0f){
+			Destroy (gameObject);
+			Instantiate(explosion, transform.position,Quaternion.identity);
+		}
+	}
+
+	public void InitBomb(Transform player) {
+		myPlayer = player;
+	}
+}
