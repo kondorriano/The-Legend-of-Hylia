f337cdd65126bfa15b87576f50830337eac3687a
kondorriano <nicobito@gmail.com>
2015-04-30 05:34:33 +0200

    Fixed PressedUnpressed Button

--- Hylia/Assets/Scripts/Dungeon/ButtonPressUnPress.cs
+++ Hylia/Assets/Scripts/Dungeon/ButtonPressUnPress.cs
@@ -15,29 +15,31 @@ public class ButtonPressUnPress : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D c) {
 		if (c.gameObject.tag == "Player1" || c.gameObject.tag == "Player2" || c.gameObject.tag == "Block") {
 			++numberPressing;
+			if(numberPressing == 1) {
+				active = true;
+				GetComponent<Animator> ().SetBool ("Active", active);
+				GetComponent<AudioSource> ().Play ();
+				activateStuff ();
+			}
 		}		
 
 
-		if(numberPressing == 1) {
-			active = true;
-			GetComponent<Animator> ().SetBool ("Active", active);
-			GetComponent<AudioSource> ().Play ();
-			activateStuff ();
-		}
+
 		
 	}
 
 	void OnTriggerExit2D(Collider2D c) {
 		if (c.gameObject.tag == "Player1" || c.gameObject.tag == "Player2" || c.gameObject.tag == "Block") {
 			--numberPressing;
+			if(numberPressing == 0) {
+				active = false;
+				GetComponent<Animator> ().SetBool ("Active", active);
+				activateStuff ();
+			}
 		}		
 
 
-		if(numberPressing == 0) {
-			active = false;
-			GetComponent<Animator> ().SetBool ("Active", active);
-			activateStuff ();
-		}
+
 		
 	}
 
